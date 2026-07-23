9469fe0313165fc73781fde917d8cdd08f40cc8f
Achal-03 <128568605+Achal-03@users.noreply.github.com>
2026-07-23 22:31:02 +0530

    Add Homework_2132A.cpp for string manipulation task

---
+++ Rating - 800/Homework_2132A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    for(int test=0; test<t; test++){
+        int n;
+        cin >> n;
+        string a;
+        cin >> a;
+        int m;
+        cin >> m;
+        string b, c;
+        cin >> b >> c;
+
+        for(int i=0; i<m; i++){
+            if(c[i] == 'V'){
+                a = b[i] + a;
+            }
+            else{
+                a += b[i];
+            }
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
